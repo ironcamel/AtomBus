adb1882810a868ef368e5bcc398a777c555289c9
Naveed Massjouni <naveedm9@gmail.com>
2010-11-14 04:50:41 +0000

    Added MOTIVATION section to docs.

--- lib/AtomMQ.pm
+++ lib/AtomMQ.pm
@@ -102,23 +102,16 @@ sub new_post {
 
 =head1 DESCRIPTION
 
-An atompub server that supports the message queue/bus model.
+AtomMQ is an atompub server that supports the message queue/bus model.
 Throughout this document, I will use the term message when refering to an atom
 feed entry, since the point of this module is to use atompub for messaging.
 AtomMQ extends Inoue's L<Atompub::Server> which extends Miyagawa's
 L<XML::Atom::Server>.
 Can you feel the love already?
 
-Usage:
-
-    #!/usr/bin/perl
-    use AtomMQ;
-    my $dsn = 'dbi:SQLite:dbname=foo.db';
-    my $server = AtomMQ->new(feed => 'MyCoolFeed', dsn => $dsn);
-    $server->run;
-
-Just drop the above file in your cgi-bin folder on your web server, and you will
-have a shiny new atompub server with a feed titled MyCoolFeed.
+To get started, just copy the code from the </SYNOPSIS> to a file and drop it
+into the cgi-bin folder on your web server, and you will have a shiny new
+atompub server with a feed titled MyCoolFeed.
 It can also be run via mod_perl on apache.
 To create more feeds, just copy that file and change 'MyCoolFeed' to
 'MyOtherFeed'.
@@ -175,6 +168,31 @@ This table will be created automagically for you if it doesn't already exist.
 If you want to create it yourself, see L<AtomMQ::Schema::Result::AtomMQEntry>
 for the schema.  All databases supported by L<DBIx::Class> are supported,
 which are most major databases including postgresql, sqlite and mysql.
+
+=head1 MOTIVATION
+
+Why did I create this module?
+I am a big fan of messaging systems and make heavy use of them to easily create
+scalable systems.
+A traditional message broker is great for creating message queues.
+But once a consumer reads a message off of a queue, it is gone.
+I needed a system to publish events such that multiple heterogeneous services
+could subscribe to them.
+So I really needed a message bus, not a message queue.
+I know for example I could have used something called topics in ActiveMQ,
+but they are extremely flakey in my experience.
+Actually, I have found ActiveMQ to be broken in general.
+An instance I manage has to be restarted at least twice a week.
+AtomMQ on the other hand will be extremely stable, because it is so simple.
+It is just a view into a database table.
+As long as your database and web server are up, AtomMQ will be there for you.
+It will not let you down.
+And there are all sorts of ways to add redundency to databases and web heads.
+Another advantage of using an atompub server is that atompub is an rfc standard.
+Everyone already has a client for it, their browser.
+Aren't standards are great!  
+By the way, if you just need message queues try L<POE::Component::MessageQueue>.
+It rocks. If you need a message bus, give AtomMQ a shot.
 
 =cut
 
